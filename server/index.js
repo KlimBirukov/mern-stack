cd34dcc46a76b9b0184b48e5746e0d2698912377
KlimBirukov <Klimbirukov0@gmail.com>
2022-08-12 21:08:01 +0300

    Added a registration method

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 import mongoose from "mongoose";
+import {validationResult} from "express-validator";
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import {registerValidation} from "./validators/auth.js";
+import UserModel from './models/User.js'
 
 
 mongoose
@@ -9,11 +15,46 @@ mongoose
 
 
 const app = express()
-app.use(express.json)
+app.use(express.json())
 
 app.listen(4444, (err) => {
-    if(err) {
+    if (err) {
         return console.log(err)
     }
     console.log('Server is working.')
 })
+
+app.post('/auth/register', registerValidation, async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10)
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = new UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        });
+        const user = await doc.save()
+
+        const token = jwt.sign({
+            _id: user._id,
+        }, 'secret123', {
+            expiresIn: '30d',
+        });
+        const {passwordHash, ...userData} = user._doc
+        res.json({...userData, token})
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({
+            message: 'Registration failed.'
+        })
+    }
+})
+
