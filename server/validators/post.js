122178926a8aa87b30bda211a6432b8a8d707a84
KlimBirukov <Klimbirukov0@gmail.com>
2022-08-17 13:50:11 +0300

    Added post validation

---
+++ server/validators/post.js
@@ -0,0 +1,8 @@
+import {body} from 'express-validator'
+
+export const postCreateValidation = [
+    body('title', 'Enter article title.').isLength({min: 3}).isString,
+    body('text', 'Enter article text.').isLength({min: 3}).isString(),
+    body('tags', 'Invalid tags format, specify array.').optional().isString(),
+    body('imageUrl', 'Invalid avatar ref.').optional().isString()
+]
